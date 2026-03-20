52c7d28e38a3911287b72179ed4f631848e6bd34
corey somers <coreysomers95@gmail.com>
2026-03-19 23:37:41 -0700

    Initial Commit

---
+++ moviefinder/moviefinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviefinderApp.swift
+//  moviefinder
+//
+//  Created by Corey Somers on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct moviefinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
